ac18a6d1f9b40b5bf65778d4f2f0c313a651938c
Fer Hernandez <fh.1111@gmail.com>
2019-03-03 22:26:23 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,7 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
@@ -24,7 +24,7 @@ class SearchableMovieReviewsContainer extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    fetch(URL.concat(this.state.searchTerm))
+    fetch(BASE_URL.concat(this.state.searchTerm))
       .then(res => res.json())
       .then(res => this.setState({ reviews: res.results }));
   }
